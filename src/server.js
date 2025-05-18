1bed65038da94511e2f2a41ef09e915f00716690
silhquette <muhammadqiyam03@gmail.com>
2025-05-18 16:18:08 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const Routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(Routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
